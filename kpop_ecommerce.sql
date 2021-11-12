f58c16ce5c8321d58fe6acb0b287f68260b66d30
axelnatashia <61000589+axelnatashia@users.noreply.github.com>
2021-11-12 09:32:08 +0800

    Add files via upload

--- kpop_ecommerce.sql
+++ kpop_ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 21, 2021 at 07:25 AM
+-- Generation Time: Nov 12, 2021 at 12:59 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
@@ -44,9 +44,7 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `name`, `phone_number`, `email`, `avatar`, `username`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin', 'admin@gmail.com', NULL, 'admin', '$2y$10$v0iqiBnzPPAph9j5r9PTfe/7IjHOaZCaLV1tksnQwonkbHKTBNoqa', '2021-05-14 06:30:41', '2021-05-14 06:30:41'),
-(2, 'admin2', 'admin2', 'admin2@gmail.com', NULL, 'admin2', '$2y$10$.4x5dCIN0KNkaPkKDa80seJ4sRaFdiiL2shUXW8zEmj2yyzeTniRO', '2021-05-14 06:30:58', '2021-05-14 06:30:58'),
-(3, 'admin3', 'admin3', 'admin3@gmail.com', NULL, 'admin3', '$2y$10$iFoWi74eq7r3gl.dpF58Bu6X4hsuDXb5hiJTIivR8.2pskcbmWLjG', '2021-05-14 06:31:19', '2021-05-14 06:31:19');
+(1, 'admin', '087855117273', 'admin.my@gmail.com', '1636481343.jpg', 'admin', '$2y$10$0AUcC4xF8YSV9ZteoDR6Ge5FWKHAKOK2KVv3LgdGC5l65taqgE50m', '2021-11-09 10:09:04', '2021-11-09 10:09:04');
 
 -- --------------------------------------------------------
 
@@ -73,11 +71,8 @@ CREATE TABLE `buyers` (
 --
 
 INSERT INTO `buyers` (`id`, `name`, `phone_number`, `email`, `address`, `province`, `avatar`, `username`, `password`, `created_at`, `updated_at`) VALUES
-(7, 'Merta Yoga', '087855117273', 'mertayoga17.my@gmail.com', NULL, NULL, NULL, 'w', '$2y$10$zVErnjPqSZzaFLlCOVjIGe329yGcPRlzyfYUrCTme1if6FhZc.uLa', '2021-04-20 16:42:55', '2021-04-20 16:42:55'),
-(8, 'febi', '087478596', 'febi@gmail.com', 'denpasar', 'jawa timur', '1619072896.jpg', 'febi', '$2y$10$eJJgewXRjZBf4uccqR1oHuW1TExBcyho4clGZEjzbrVAnFD.LSQGe', '2021-04-21 22:25:51', '2021-04-21 22:28:16'),
-(9, 'buyer', '123456789', 'buyer@gmail.com', 'buyer address', 'bali', '1621048180.png', 'buyer', '$2y$10$.By0Vr3VK44TzywIrn86Peq8H9bs/8qKJuUNP.CmmDqrPs0p7pc.m', '2021-05-14 19:08:39', '2021-05-14 19:09:40'),
-(10, 'buyer2', '123456789', 'buyer2@gmail.com', 'address', 'sumatera utara', '1628062174.png', 'buyer2', '$2y$10$9zNoq7I6o1896pO14iRvReoX5TEbkddc7AVq5pOrm.Y6Ip87AiM/.', '2021-05-14 19:08:52', '2021-08-03 23:29:34'),
-(11, 'buyer3', '123456789', 'buyer3@gmail.com', 'wade', 'jawa timur', NULL, 'buyer3', '$2y$10$..IwpQqXD1LWy0SwG4Tc0eau0AUUGI6ss0Q9W9e7SuoeKFswzi52O', '2021-05-14 19:09:11', '2021-08-08 06:20:01');
+(1, 'buyer', '087855117273', 'buyer.my@gmail.com', 'addrs', 'sumatera utara', '1636483296.png', 'buyer', '$2y$10$fVeDTwCyiCrCIIJtpIgkBeURdOCUQdEnBD9P7CnpqHQ66OodEXToO', '2021-11-09 10:19:00', '2021-11-11 02:51:42'),
+(2, 'buyer2', '087855117273', 'buyer2.my@gmail.com', 'addres', 'sumatera barat', NULL, 'buyer2', '$2y$10$m.YB6ten.X3.Hf5k4CSrkOnPwrTp9n1J6C9x0xYXWqXJqT5y165WC', '2021-11-09 10:19:29', '2021-11-11 02:52:04');
 
 -- --------------------------------------------------------
 
@@ -94,6 +89,57 @@ CREATE TABLE `carts` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `carts`
+--
+
+INSERT INTO `carts` (`id`, `buyer_id`, `seller_product_id`, `qty`, `created_at`, `updated_at`) VALUES
+(11, 1, 3, 1, '2021-11-11 07:05:42', '2021-11-11 07:05:42');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chats`
+--
+
+CREATE TABLE `chats` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `admin_id` int(11) DEFAULT NULL,
+  `seller_id` int(11) DEFAULT NULL,
+  `buyer_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `chats`
+--
+
+INSERT INTO `chats` (`id`, `admin_id`, `seller_id`, `buyer_id`, `created_at`, `updated_at`) VALUES
+(1, 1, NULL, 1, '2021-11-11 14:50:19', '2021-11-11 14:50:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat_details`
+--
+
+CREATE TABLE `chat_details` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `chat_id` int(11) NOT NULL,
+  `img` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `text` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `chat_details`
+--
+
+INSERT INTO `chat_details` (`id`, `chat_id`, `img`, `text`, `created_at`, `updated_at`) VALUES
+(1, 1, NULL, 'hello', '2021-11-11 15:56:40', '2021-11-11 15:56:40');
+
 -- --------------------------------------------------------
 
 --
@@ -105,7 +151,7 @@ CREATE TABLE `checkouts` (
   `code` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `total_payment` int(11) DEFAULT NULL,
-  `note` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `already_paid` int(11) NOT NULL DEFAULT 0,
   `arrival_date` date DEFAULT NULL,
   `payment_method_id` int(11) NOT NULL,
   `buyer_id` int(11) NOT NULL,
@@ -117,9 +163,9 @@ CREATE TABLE `checkouts` (
 -- Dumping data for table `checkouts`
 --
 
-INSERT INTO `checkouts` (`id`, `code`, `status`, `total_payment`, `note`, `arrival_date`, `payment_method_id`, `buyer_id`, `created_at`, `updated_at`) VALUES
-(2, 'TRCT20210815002', 'in progress', 55000, NULL, NULL, 3, 8, '2021-08-14 17:43:03', '2021-08-14 17:43:03'),
-(3, 'TRCT20210815003', 'in progress', 451000, NULL, NULL, 1, 8, '2021-08-14 20:35:11', '2021-08-14 20:35:11');
+INSERT INTO `checkouts` (`id`, `code`, `status`, `total_payment`, `already_paid`, `arrival_date`, `payment_method_id`, `buyer_id`, `created_at`, `updated_at`) VALUES
+(5, 'TRCT20211109005', 'in progress', 300000, 150000, NULL, 1, 1, '2021-11-09 11:23:55', '2021-11-09 11:23:55'),
+(8, 'TRCT20211109008', 'in progress', 320000, 160000, NULL, 1, 2, '2021-11-09 12:27:19', '2021-11-09 12:27:19');
 
 -- --------------------------------------------------------
 
@@ -145,9 +191,9 @@ CREATE TABLE `checkout_details` (
 --
 
 INSERT INTO `checkout_details` (`id`, `checkout_id`, `note`, `seller_product_id`, `qty`, `price`, `total_price`, `status`, `created_at`, `updated_at`) VALUES
-(2, '2', NULL, 3, 1, 50000, 50000, 'in progress', '2021-08-14 17:43:03', '2021-08-14 17:43:03'),
-(3, '2', NULL, 4, 1, 5000, 5000, 'in progress', '2021-08-14 17:43:03', '2021-08-14 17:43:03'),
-(4, '3', NULL, 5, 1, 451000, 451000, 'in progress', '2021-08-14 20:35:11', '2021-08-14 20:35:11');
+(6, '5', NULL, 3, 1, 300000, 300000, 'in progress', '2021-11-09 11:23:55', '2021-11-09 11:23:55'),
+(9, '8', NULL, 1, 1, 120000, 120000, 'in delivery', '2021-11-09 12:27:19', '2021-11-09 13:04:04'),
+(10, '8', NULL, 2, 1, 200000, 200000, 'in progress', '2021-11-09 12:27:19', '2021-11-09 12:27:19');
 
 -- --------------------------------------------------------
 
@@ -182,20 +228,22 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(19, '2014_10_12_000000_create_users_table', 1),
-(20, '2014_10_12_100000_create_password_resets_table', 1),
-(21, '2019_08_19_000000_create_failed_jobs_table', 1),
-(22, '2021_04_20_011638_create_sellers_table', 1),
-(23, '2021_04_20_201606_create_buyers_table', 1),
-(24, '2021_04_20_202539_create_admins_table', 1),
-(26, '2021_04_26_033146_create_seller_products_table', 2),
-(27, '2021_05_14_045637_create_serial_numbers_table', 3),
-(32, '2021_05_15_020713_create_wishlists_table', 4),
-(33, '2021_05_15_020904_create_carts_table', 4),
-(34, '2021_08_11_134442_create_paylaters_table', 5),
-(35, '2021_08_13_143213_create_payment_methods_table', 5),
-(42, '2021_08_14_122818_create_checkouts_table', 6),
-(43, '2021_08_14_122831_create_checkout_details_table', 6);
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2021_04_20_011638_create_sellers_table', 1),
+(5, '2021_04_20_201606_create_buyers_table', 1),
+(6, '2021_04_20_202539_create_admins_table', 1),
+(7, '2021_04_26_033146_create_seller_products_table', 1),
+(8, '2021_05_14_045637_create_serial_numbers_table', 1),
+(9, '2021_05_15_020713_create_wishlists_table', 1),
+(10, '2021_05_15_020904_create_carts_table', 1),
+(11, '2021_08_11_134442_create_paylaters_table', 1),
+(12, '2021_08_13_143213_create_payment_methods_table', 1),
+(13, '2021_08_14_122818_create_checkouts_table', 1),
+(14, '2021_08_14_122831_create_checkout_details_table', 1),
+(15, '2021_11_11_140934_create_chats_table', 2),
+(16, '2021_11_11_140942_create_chat_details_table', 2);
 
 -- --------------------------------------------------------
 
@@ -232,7 +280,7 @@ CREATE TABLE `paylaters` (
 --
 
 INSERT INTO `paylaters` (`id`, `buyer_id`, `balance`, `status`, `identity_number`, `identity_card_img`, `selfie`, `created_at`, `updated_at`) VALUES
-(1, 8, 2000000, 'confirm', '6666', 'id_1628999956.jpg', 'selfie_1628999956.jpg', '2021-08-14 19:59:17', '2021-08-14 20:34:33');
+(1, 1, 1030000, 'confirm', '5555', 'id_1636483660.jpg', 'selfie_1636483660.jpg', '2021-11-09 10:47:40', '2021-11-09 12:27:19');
 
 -- --------------------------------------------------------
 
@@ -252,12 +300,9 @@ CREATE TABLE `payment_methods` (
 --
 
 INSERT INTO `payment_methods` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'Pay Later', '2021-08-13 07:53:07', '2021-08-14 04:12:56'),
-(3, 'OVO', '2021-08-14 04:02:24', '2021-08-14 04:02:24'),
-(4, 'Indomaret', '2021-08-14 04:02:30', '2021-08-14 04:02:30'),
-(5, 'Alfamart', '2021-08-14 04:02:35', '2021-08-14 04:02:35'),
-(6, 'Dana', '2021-08-14 04:02:40', '2021-08-14 04:02:40'),
-(7, 'Bank Transfer', '2021-08-14 04:02:52', '2021-08-14 04:02:52');
+(1, 'Pay Later', '2021-11-09 10:14:12', '2021-11-09 10:14:12'),
+(2, 'Isaku Indomaret', '2021-11-09 10:14:20', '2021-11-09 10:14:20'),
+(3, 'Transfer Rekening', '2021-11-09 10:18:22', '2021-11-09 10:18:22');
 
 -- --------------------------------------------------------
 
@@ -285,10 +330,8 @@ CREATE TABLE `sellers` (
 --
 
 INSERT INTO `sellers` (`id`, `name`, `description`, `phone_number`, `email`, `address`, `province`, `avatar`, `username`, `password`, `created_at`, `updated_at`) VALUES
-(6, 'Merta Yoga', 'descsss', '0878551172732', 'mertayoga17.my@gmail.com', 'Ahmad Yani Street, Anugrah VII Number 17', 'sumatera utara', '1618985286.png', 'merta', '$2y$10$oZbeqqNCr0Q2VU6MCBdpA.sTrrTbJdXlmdEOAPL3o/AfqxLVq.otO', '2021-04-20 20:57:55', '2021-05-14 05:33:06'),
-(7, 'axel', 'desc', '0878547858', 'axel@gmail.com', 'denpasar', 'sumatera utara', '1619072709.png', 'axel', '$2y$10$.q7166SfWAxpK0d/qa.9du11bFV8ofkIfZHVq8Mm0eEQBGbItjj0.', '2021-04-21 22:23:58', '2021-04-21 22:25:10'),
-(8, 'seller', 'seller desc', '834564', 'seller@gmail.com', 'seller address', 'aceh', '1620998693.png', 'seller', '$2y$10$KxT1EruKE7ektYSHV2jEh.1NkeBmcr.Es4LRLyDQSviS3fP6NCOUq', '2021-05-14 05:24:53', '2021-05-14 23:28:47'),
-(9, 'seller22', 'seller22 desc', '8345642', 'seller22@gmail.com', 'seller22 address', 'maluku utara', '1620998720.png', 'seller2', '$2y$10$7y8Lh4rdAfR25uFNBIQ92eHppZzo8BVwX/wzDB9nviK0d6hIokUyG', '2021-05-14 05:25:20', '2021-05-14 05:28:17');
+(1, 'seller', 'desc', '087855117273', 'seller.my@gmail.com', 'address', 'bali', '1636482429.png', 'seller', '$2y$10$USo7mqXuhDuUqJDQJ.oEiOAiNDhVAbSD2vfAnYfa3D9/qhkxlvVgW', '2021-11-09 10:19:49', '2021-11-09 11:52:51'),
+(2, 'seller2', 'desc', '087855117273', 'seller2@gmail.com', 'addrs', 'bali', '1636483063.jpg', 'seller2', '$2y$10$EGocvLR7VS3lh5qmwgYay.xwq.w5TZlryp79Zjz.iZCkKYjtNE46S', '2021-11-09 10:20:13', '2021-11-09 11:54:22');
 
 -- --------------------------------------------------------
 
@@ -313,11 +356,9 @@ CREATE TABLE `seller_products` (
 --
 
 INSERT INTO `seller_products` (`id`, `name`, `description`, `price`, `total`, `image`, `seller_id`, `created_at`, `updated_at`) VALUES
-(1, 'item 1', 'desc 1', 5000, 3, '1621057978.jpg', 6, '2021-05-04 19:55:06', '2021-05-14 21:52:59'),
-(2, 'item 2', 'desc', 50000, 1, '1621057987.png', 6, '2021-05-14 19:41:01', '2021-05-14 21:53:07'),
-(3, 'item 3', 'desc', 50000, 1, '1621057994.jpg', 6, '2021-05-14 19:41:53', '2021-05-14 21:53:15'),
-(4, 'seller item 1', 'desc', 5000, 1, '1621061309.jpg', 8, '2021-05-14 22:48:29', '2021-05-14 22:48:29'),
-(5, 'seller item 22', 'desc', 451000, 2, '1621063662.jpg', 8, '2021-05-14 23:27:42', '2021-05-14 23:28:35');
+(1, 'product 1 seller', 'desc', 120000, 1, '1636482483.png', 1, '2021-11-09 10:28:04', '2021-11-09 10:28:04'),
+(2, 'product 2 seller', 'desc', 200000, 1, '1636482583.png', 1, '2021-11-09 10:29:43', '2021-11-09 10:29:57'),
+(3, 'product 1 seller2', 'desc', 300000, 1, '1636482968.png', 2, '2021-11-09 10:36:08', '2021-11-09 10:36:08');
 
 -- --------------------------------------------------------
 
@@ -338,12 +379,10 @@ CREATE TABLE `serial_numbers` (
 --
 
 INSERT INTO `serial_numbers` (`id`, `code`, `status`, `created_at`, `updated_at`) VALUES
-(1, '123', 0, '2021-05-14 19:33:39', '2021-05-14 23:33:32'),
-(2, '1234', 1, '2021-05-14 19:33:43', '2021-05-14 19:41:53'),
-(3, '124', 1, '2021-05-14 19:33:47', '2021-05-14 22:48:30'),
-(4, '125', 0, '2021-05-14 19:33:49', '2021-05-14 23:27:42'),
-(5, '126', 0, '2021-05-14 19:33:52', '2021-05-14 19:33:52'),
-(6, '127', 0, '2021-05-14 23:33:19', '2021-05-14 23:33:19');
+(1, '123', 1, '2021-11-09 10:09:20', '2021-11-09 10:28:04'),
+(2, '456', 1, '2021-11-09 10:09:26', '2021-11-09 10:29:43'),
+(3, '789', 1, '2021-11-09 10:09:31', '2021-11-09 10:36:08'),
+(4, '111', 0, '2021-11-09 10:51:05', '2021-11-09 10:51:05');
 
 -- --------------------------------------------------------
 
@@ -381,11 +420,7 @@ CREATE TABLE `wishlists` (
 --
 
 INSERT INTO `wishlists` (`id`, `buyer_id`, `seller_product_id`, `created_at`, `updated_at`) VALUES
-(1, 9, 3, '2021-05-14 21:55:47', '2021-05-14 21:55:47'),
-(2, 10, 5, '2021-08-04 00:01:17', '2021-08-04 00:01:17'),
-(3, 10, 4, '2021-08-04 00:01:24', '2021-08-04 00:01:24'),
-(4, 8, 3, '2021-08-08 05:16:34', '2021-08-08 05:16:34'),
-(5, 8, 5, '2021-08-08 05:16:58', '2021-08-08 05:16:58');
+(2, 1, 2, '2021-11-09 10:42:55', '2021-11-09 10:42:55');
 
 --
 -- Indexes for dumped tables
@@ -409,6 +444,18 @@ ALTER TABLE `buyers`
 -- Indexes for table `carts`
 --
 ALTER TABLE `carts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `chats`
+--
+ALTER TABLE `chats`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `chat_details`
+--
+ALTER TABLE `chat_details`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -496,31 +543,43 @@ ALTER TABLE `wishlists`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `buyers`
 --
 ALTER TABLE `buyers`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `carts`
 --
 ALTER TABLE `carts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `chats`
+--
+ALTER TABLE `chats`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `chat_details`
+--
+ALTER TABLE `chat_details`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `checkouts`
 --
 ALTER TABLE `checkouts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `checkout_details`
 --
 ALTER TABLE `checkout_details`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -532,7 +591,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `paylaters`
@@ -544,25 +603,25 @@ ALTER TABLE `paylaters`
 -- AUTO_INCREMENT for table `payment_methods`
 --
 ALTER TABLE `payment_methods`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `sellers`
 --
 ALTER TABLE `sellers`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `seller_products`
 --
 ALTER TABLE `seller_products`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `serial_numbers`
 --
 ALTER TABLE `serial_numbers`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -574,7 +633,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `wishlists`
 --
 ALTER TABLE `wishlists`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
