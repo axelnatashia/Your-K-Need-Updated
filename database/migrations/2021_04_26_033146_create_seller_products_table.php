528d7a4f046effa20cf1df8903fc9b0bef5cef70
axelnatashia <61000589+axelnatashia@users.noreply.github.com>
2021-11-12 14:05:33 +0800

    database

---
+++ database/migrations/2021_04_26_033146_create_seller_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSellerProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seller_products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('price');
+            $table->integer('total');
+            // $table->string('serial_number');
+            $table->string('image');
+            $table->integer('seller_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seller_products');
+    }
+}
