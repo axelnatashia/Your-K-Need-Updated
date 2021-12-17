715f42aaa6a606f3994d620613c1935294f1e2c6
axelnatashia <61000589+axelnatashia@users.noreply.github.com>
2021-12-17 11:04:55 +0800

    Delete Helpers directory

--- Helpers/ReturnGreeting.php
+++
@@ -1,8 +0,0 @@
-<?php
-    function returnGreeting(){
-        date_default_timezone_set('Asia/Jakarta');
-        $hour = date('G');
-        $dayTerm = ($hour > 17) ? "Evening" : (($hour > 10) ? "Afternoon" : "Morning");
-        return "Good " . $dayTerm;
-    }
-?>
--- Helpers/ReturnResponseApi.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function responseApi($status, $data, $msg){
-        return response()->json([
-            'status' => $status,
-            'data' => $data,
-            'msg' => $msg
-        ]);
-    }
-?>
